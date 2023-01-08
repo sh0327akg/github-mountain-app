c0fb03280daa25bbff1e3c1d7af6938d45dce10c
sh0327akg <shota.gk0327m.tf@gmail.com>
2023-01-08 12:53:36 +0900

    Postコントローラーの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'posts/index'
+  get 'posts/new'
+  get 'posts/create'
   root 'home#top'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
