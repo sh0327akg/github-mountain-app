fc4163374f0653da21b7fefca3059e778bdb1779
sh0327akg <shota.gk0327m.tf@gmail.com>
2023-01-08 12:57:19 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
-  get 'posts/index'
-  get 'posts/new'
-  get 'posts/create'
   root 'home#top'
+  resources :posts, only: %i[index new create]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
