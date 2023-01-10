4d3265f8fc693a692a38b6d3e84f2e46c9c80f84
sh0327akg <shota.gk0327m.tf@gmail.com>
2023-01-10 14:01:52 +0900

    草を取得するメソッドの作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,5 +7,26 @@ class PostsController < ApplicationController
   end
 
   def create
+    @post = Post.new(post_params)
+    grass = @post.scrape_github
+    Mountain.where()
+    @post.merge(mountain_id: Mountain.id)
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:account_name)
+  end
+
+  def scrape_github
+    agent = Mechanize.new
+    page = agent.get("https://github.com/#{account_name}")
+    grass = page.search('//h2[@class = "f4 text-normal mb-2"]').inner_text
+    grass.delete!('in the last year')
+    grass.delete!('cobuo')
+    grass.gsub(/[\r\n]/,"")
+
+    return grass.to_i
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,16 @@
 class Post < ApplicationRecord
   belongs_to :mountain_list
+
+  validates :account_name, presence: true, format: { with: /\A[0-9a-zA-Z-_]*\z/, message: "半角英数字のみが使えます"}
+
+  validate :exist_on_github
+
+  private
+
+  def exist_on_github
+    target_uri = "https://github.com/#{self.name}"
+    uri = URI.parse(target_uri)
+    response = Net::HTTP.get_response(uri)
+    errors.add(:account_name, "GitHubに存在しないアカウント名です") if response.code == "404"
+  end
 end
